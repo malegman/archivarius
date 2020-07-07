7cd65df66324870f4fa156a69ccd73db568ece08
malegman <smegovic@gmail.com>
2020-07-07 16:24:16 +0500

    Инициализировать проект

---
+++ src/test/java/com/gmail/smegovic/archivarius/ArchivariusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.smegovic.archivarius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivariusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
